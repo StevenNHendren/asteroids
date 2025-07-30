80fbeeb4d45803945f1f429e762dee9d297bc17e
StevenNHendren <tebtengri@gmail.com>
2025-07-30 15:44:35 +0000

    player ship can move

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 BLACK = (0, 0, 0)
 
@@ -12,6 +13,11 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     ret = pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    gclock = pygame.time.Clock()
+    dt = 0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player1 = Player(x, y)
 
     while True:
         for event in pygame.event.get():
@@ -19,7 +25,12 @@ def main():
                 return
         #rect = pygame.Surface.fill(color(0,0,0))
         screen.fill(BLACK)
+        player1.update(dt)
+        player1.draw(screen)
         pygame.display.flip()
+        tick_ret = gclock.tick(60)
+        dt = tick_ret / 1000
+        
 
 if __name__ == "__main__":
     main()
